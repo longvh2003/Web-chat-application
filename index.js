8512a39b28080396e35508d8c518d9eda2c4c435
lawlietsnow <thotang19@gmail.com>
2019-10-13 18:58:21 +0700

    room

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 var express=require('express');
 var app=express();
+var bodyParser=require('body-parser');
 
 app.set('view engine','pug');
 app.set('views','./views');
 
+app.use(bodyParser.urlencoded({extended:true}));
+
 app.get('/',function(req,res){
-	res.render('index');
+	res.render('joinForm');
+});
+
+app.post('/chat-room',function(req,res){
+	res.render('index',{
+		dsa:req.body.asd
+	});
 });
 
 var server=app.listen(3000,()=>{console.log('running...');});
@@ -15,8 +24,11 @@ var io = socket(server);
 
 io.on('connection',function(socket){
 	console.log('connected');
+	socket.on('room',data=>{
+		socket.join(data);
+	});
 	socket.on('sendbackserver',function(data){
 		console.log('nhan data thanh cong');
-		io.sockets.emit('sendtoclients',data);
+		io.sockets.in(data.roomNumber).emit('sendtoclients',data);
 	});
 });
