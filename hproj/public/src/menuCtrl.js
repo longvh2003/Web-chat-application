b45ae7baac4b45ba5d3eaa0f4feff5b77890e79b
lawlietsnow <thotang19@gmail.com>
2019-11-07 15:45:01 +0700

    hiển thị phòng chat khi click vào tên bạn

--- hproj/public/src/menuCtrl.js
+++ hproj/public/src/menuCtrl.js
@@ -1,4 +1,6 @@
 function menuCtrl($rootScope, $scope, $location, $http, $window){
+    // $scope.listFriends=[1,2,3,4,5,6,7,8,9,10];
+
     $rootScope.$on('menu-clicked', ()=>{
         $scope.myButton = !$scope.myButton;
     })
@@ -6,6 +8,12 @@ function menuCtrl($rootScope, $scope, $location, $http, $window){
         if(index==2){    
             $location.path('/friends');
             console.log(index);
+            $http({
+                method:'GET',
+                url:'addFriends',
+            }).then(res=>{//res.data chuua res.data[i].chatroom_id
+                $rootScope.listFriends=res.data;
+            });
         }
         else if(index==4){
             console.log('logout');
@@ -17,7 +25,17 @@ function menuCtrl($rootScope, $scope, $location, $http, $window){
             });
         }
     }
+    $scope.showEl=element=>{
+        console.log(element);
+        $location.path('/chat/'+element.chatroom_id);
+    }
     $scope.display=()=>{
+        console.log($scope.asd);
+        $http({
+            method:'POST',
+            url:'/addFriends',
+            data:{username:$scope.asd}
+        });
         $window.location.reload();
     }
 
