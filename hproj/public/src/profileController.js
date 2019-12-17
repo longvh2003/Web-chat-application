13e4875a163b5abb257dd9dd7e53d3ea593d51c2
lawlietsnow <thotang19@gmail.com>
2019-12-17 20:26:38 +0700

    load anh vao profile

--- hproj/public/src/profileController.js
+++ hproj/public/src/profileController.js
@@ -1,16 +1,16 @@
-function ProfileCtrl($scope, $rootScope){
+function ProfileCtrl($scope, $rootScope,$http){
     $rootScope.$on('username', ()=>{
         $scope.username = $rootScope.username;
     })
     $scope.password = 'sample';
     $scope.hidePassword = true;
     $scope.myDate = new Date();
-
-
-    $scope.changeAvatar= function(me){
-        if (me.files && me.files[0]) {
-            let url = URL.createObjectURL(me.files[0]); // set src to blob url
-            angular.element('.avatar').css('background-image', 'url(' + url +')');
-        }
-    }
+    $http({
+            method:'GET',
+            url:'/getUserSession'
+        }).then(res=>{
+        console.log("le trung kien"+res.data.user.userId);
+        // document.getElementById("myImage").setAttribute("src", '/userAvatar/'+res.data.user.userId+'.jpg');
+        angular.element('#myImage').attr('src','userAvatar/1.jpg');
+    });
 }
